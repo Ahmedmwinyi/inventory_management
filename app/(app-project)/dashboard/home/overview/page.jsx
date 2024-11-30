7c00aab6bb805a896a64941a8b554ffa137433a4
Ahmedmwinyi <Madiylal@gmail.com>
2024-12-01 00:30:58 +0300

    Continous commit to this project

--- app/(app-project)/dashboard/home/overview/page.jsx
+++ app/(app-project)/dashboard/home/overview/page.jsx
@@ -1,8 +1,10 @@
+import DashboardBanner from '@/components/dashboard/DashboardBanner'
 import React from 'react'
 
 export default function Dashboard() {
   return (
     <div>
+      <DashboardBanner />
       <h2>Dashboard</h2>
     </div>
   )
--- components/dashboard/HomeNavBar.jsx
+++ components/dashboard/HomeNavBar.jsx
@@ -1,21 +1,60 @@
+"use client";
 import { Building2 } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 export default function HomeNavBar() {
+  const pathname = usePathname();
+  console.log(pathname);
+  const navLinks = [
+    {
+      title: "Dashboard",
+      href: "/dashboard/home/overview",
+    },
+    {
+      title: "Getting Started",
+      href: "/dashboard/home/getting-started",
+    },
+    {
+      title: "Recent Updates",
+      href: "/dashboard/home/updates",
+    },
+    {
+      title: "Announcements",
+      href: "/dashboard/home/announcement",
+    },
+  ];
   return (
     <div>
-      <div className="h-32 bg-green-300 p-5">
+      <div className="h-32 p-5 header-bg bg-slate-50 border-b border-slate-300">
         <div className="flex space-x-3">
           <div className="flex w-12 h-12 rounded-lg bg-white items-center justify-center">
             <Building2 />
           </div>
           <div className="flex flex-col">
-            <p>Hello, AHMED JANU</p>
+            <p className="text-slate-700 font-semibold">Hello, AHMED JANU</p>
             <span className="text-sm">Janu</span>
           </div>
         </div>
 
-        
+        <nav className="sticky mt-6 flex space-x-4">
+          {navLinks.map((item, i) => {
+            return (
+              <Link
+                key={i}
+                href={item.href}
+                className={`${
+                  pathname === item.href
+                    ? "py-1.5 border-b-2 border-blue-600"
+                    : "py-1.5"
+                }`}
+              >
+                {item.title}
+              </Link>
+            );
+          })}
+        </nav>
       </div>
     </div>
   );
